068b649c24035c04ac444a46eb15063648560cd6
Morgan Fogarty <mofo37@gmail.com>
2017-03-20 14:29:57 -0700

    added page method

--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -1,11 +1,22 @@
 'use strict';
 
+Article.fetchAll(articleView.initIndexPage);
+
 (function(module) {
   const articleController = {};
-  // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
-  // code that used to be in index.html.
-  // Also be sure to hide all the main section elements, and reveal the #articles section:
 
+  articleController.logview = function() {
+    console.log('From articleController');
+  }
+
+  articleController.render = function() {
+    $('#about').hide();
+    $('#articles').fadeIn();
+  }
 
   module.articleController = articleController;
 })(window);
+
+  // Done/TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
+  // code that used to be in index.html.
+  // Also be sure to hide all the main section elements, and reveal the #articles section:
--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -1,6 +1,11 @@
 'use strict';
 
-// TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
+page('/about', aboutController.render);
+page('/home', articleController.render);
+
+page();
+
+// Done/TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
 
-// TODO: What function do you call to activate page.js? Fire it off now, to execute
+// Done/TODO: What function do you call to activate page.js? Fire it off now, to execute
