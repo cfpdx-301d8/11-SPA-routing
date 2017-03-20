026bcf85e0d105969cffed557ac4e5f13746ce51
Keeley Hammond <keeleymhammond@gmail.com>
2017-03-20 13:41:15 -0700

    Lab complete

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -1,9 +1,13 @@
 'use strict';
 
-(function(module) {
+(function (module) {
   const aboutController = {};
 
-  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+  // DONE TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+  aboutController.render = function () {
+    $('.tab-content').hide();
+    $('#about').fadeIn();
+  }
 
   module.aboutController = aboutController;
 })(window);
