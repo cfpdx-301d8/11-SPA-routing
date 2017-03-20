ce27f164a47dc74174991183bbce5bd10fec54b6
nicky evers <livelyevers@gmail.com>
2017-03-20 14:47:36 -0700

    got script tags fixed and fixed controllers

--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -1,10 +1,10 @@
 'use strict';
 
-// TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
+// DONE TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
 
 page('/about', aboutController.showAbout);
 page('/home', articleController.showArticles);
 
-// TODO: What function do you call to activate page.js? Fire it off now, to execute
+// DONE TODO: What function do you call to activate page.js? Fire it off now, to execute
 page();
